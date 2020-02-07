d78e615d65c086686cd953dc03c687a3af4c4a8f
Rajat Singhal <rajatsinghal564@gmail.com>
2020-02-07 21:07:55 +0530

    Cleaned up files

--- assignment1/CS17B042.cu
+++ assignment1/CS17B042.cu
@@ -1,13 +1,54 @@
-#include<stdio.h>
-#include<cuda.h>
-#include "kernels.h"
+#ifndef _KERNELS_H_
+#define _KERNELS_H_
 
 __global__ void per_row_kernel(int *in, int N) {
-    int row = threadIdx.x;
+    long long unsigned row = blockIdx.x * blockDim.x*blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
+
     // printf("%d\n", row);
-    for(int i=0; i<row; i++) {
-        int temp = in[row*N + i];
-        in[row*N + i] = in[i*N + row];
-        in[i*N + row] = temp;
+    if (row < N) {
+        for(long long unsigned i=0; i<row; i++) {
+            in[i*N + row] = in[row*N + i];
+            in[row*N + i] = 0;
+        }
+    }
+
+}
+
+__global__ void per_element_kernel(int *in, int N) {
+    long long unsigned blockId = blockIdx.x + (blockIdx.y * gridDim.x) + (blockIdx.z * gridDim.x * gridDim.y);
+    long long unsigned threadId = blockId * blockDim.x + threadIdx.x;
+
+    // printf("ThreadId: %llu\n", threadId);
+
+    int row = threadId / N;
+    int col = threadId % N;
+
+    // printf("Row: %d, Col: %d\n", row, col);
+
+    if (row < N && row < col) {
+        // printf("Row: %d, Col: %d\n", row, col);
+        // int temp = in[row*N + col];
+        in[row*N + col] = in[col*N + row];
+        in[col*N + row] = 0;
     }
 }
+
+__global__ void per_element_kernel_2D(int *in, int N) {
+    long long unsigned blockId = blockIdx.x + (blockIdx.y * gridDim.x);
+    long long unsigned threadId = (blockId * blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x;
+
+    int row = threadId / N;
+    int col = threadId % N;
+
+    // printf("Row: %d, Col: %d\n", row, col);
+
+    if (row < N && row < col) {
+        // printf("Row: %d, Col: %d\n", row, col);
+        // int temp = in[row*N + col];
+        in[row*N + col] = in[col*N + row];
+        in[col*N + row] = 0;
+    }
+}
+
+#endif
+
--- assignment1/main.cu
+++ assignment1/main.cu
@@ -1,10 +1,5 @@
-#include<stdio.h>
-#include<cuda.h>
-// #include"kernels.h"
-#include<stdlib.h>
-#include"CS17B042.cu"
-
-#define N 1000
+#include <stdio.h>
+#include "CS17B042.cu"
 
 void print_matrix(int* mat, int n) {
     for(int i=0; i<n; i++) {
@@ -15,29 +10,107 @@ void print_matrix(int* mat, int n) {
     }
 }
 
-int main() {
-    int *hmatrix, *matrix;
-    cudaMalloc(&matrix, N*N*sizeof(int));
-    hmatrix = (int *)malloc(N * N * sizeof(int));
-
-    // Initialize lower triangular matrix
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            if (i>=j)
-                hmatrix[i*N + j] = rand()%9 + 1;
-            else
-                hmatrix[i*N + j] = 0;
+bool check_if_transpose(int* mat1, int* mat2, int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(mat1[i*n + j] != mat2[j*n + i])
+                return false;
         }
     }
-    printf("Original matrix:\n");
-    print_matrix(hmatrix, N);
+    return true;
+}
 
-    cudaMemcpy(matrix, hmatrix, N*N*sizeof(int), cudaMemcpyHostToDevice);
-    per_row_kernel<<<1,N>>>(matrix, N);
-    cudaMemcpy(hmatrix, matrix, N*N*sizeof(int), cudaMemcpyDeviceToHost);
+int main()
+{
+    long long unsigned N,i,j;
+    scanf("%llu", &N);
+    int* mathost, * matdev, *resmat;
+    mathost = (int*)malloc(N * N * sizeof(int));
+    resmat = (int*)malloc(N*N*sizeof(int));
+    cudaMalloc(&matdev, N * N * sizeof(int));
 
-    printf("\n\nTransformed matrix:\n");
 
-    print_matrix(hmatrix, N);
-    return 0;
+    // print_matrix(mathost, N);
+
+    // Initialize lower triangular matrix
+    for(i=0; i<N; i++) {
+        for(j=0; j<N; j++) {
+            if (i>=j)
+                mathost[i*N + j] = rand()%9 + 1;
+            else
+                mathost[i*N + j] = 0;
+        }
+    }
+
+    // print_matrix(mathost, N);
+
+    cudaMemcpy(matdev, mathost, N * N * sizeof(int),
+                cudaMemcpyHostToDevice);
+
+
+
+    // Kernel 1
+    int griddim = ceil((float)N / 1024);
+    dim3 block1(32, 32);
+    per_row_kernel <<< griddim, block1 >>> (matdev, N);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(resmat, matdev, N * N * sizeof(int),
+               cudaMemcpyDeviceToHost);
+
+    printf("\n");
+    // print_matrix(resmat, N);
+
+    printf("Is Transpose (Kernel1): %d\n", check_if_transpose(mathost, resmat, N));
+
+
+
+    // Kernel 2
+    cudaMemcpy(matdev, mathost, N * N * sizeof(int),
+                cudaMemcpyHostToDevice);
+
+    int griddim2 = ceil((float)(N * N) / (1024 * 32 * 32));
+    dim3 grid1(griddim2, 32, 32);
+    per_element_kernel << <grid1, 1024 >> > (matdev, N);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(resmat, matdev, N * N * sizeof(int),
+                cudaMemcpyDeviceToHost);
+
+    printf("\n");
+    // print_matrix(resmat, N);
+
+    printf("Is transpose(Kernel2): %d\n", check_if_transpose(mathost, resmat, N));
+
+
+
+    // Kernel 3
+    cudaMemcpy(matdev, mathost, N * N * sizeof(int),
+                cudaMemcpyHostToDevice);
+
+    int griddim3 = ceil((float)(N * N) / (1024 * 32));
+    dim3 grid2(griddim3, 32);
+    dim3 block2(32, 32);
+    per_element_kernel_2D <<< grid2, block2 >>>(matdev, N);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(resmat, matdev, N * N * sizeof(int),
+                cudaMemcpyDeviceToHost);
+
+    printf("\n");
+    // print_matrix(resmat, N);
+
+    printf("Is transpose(Kernel3): %d\n", check_if_transpose(mathost, resmat, N));
+
+    // for (i = 0; i < N; i++)
+    // {
+    //  for (j = i + 1; j < N; j++)
+    //  {
+    //      int temp = mathost[i * N + j];
+    //      mathost[i * N + j] = mathost[j * N + i];
+    //      mathost[j * N + i] = temp;
+    //  }
+    // }
+
+    printf("\n");
 }
